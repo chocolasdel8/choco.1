fda36d894723c5a20abe5a8e9704c000ba4df1cb
enriquemamanivargas3@gmail.com <enriquemamanivargas3@gmail.com>
2023-11-14 22:15:24 -0500

    examen2,3,4,5

---
+++ src/login/RegisterForm.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { useAuth } from '../ruteo/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+// Para verificar que no registre con el mismo correo
+import { getDoc, doc } from 'firebase/firestore';
+import { auth, db } from '../conexion/firebase';
+
+function RegisterForm() {
+
+  const { register } = useAuth();             // Registra usuario
+  const { registerUser } = useAuth(); 
+  const [email, setEmail] = useState('');       // Variables para correo
+  const [password, setPassword] = useState(''); // Variable para password
+  
+  const navigate = useNavigate();               // Navegación
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      await register(email, password);
+      await registerUser(email, password);   // Verifica correo ya registrado
+      navigate('/iniciarsesion'); // Redirigir a ruta /iniciarsesion
+      console.log("Se registro usuario...xxx");
+    } catch (error) {
+      console.error('Error al registrar usuario:', error.message);
+    }
+  }
+
+  return (
+    <div  id='public'>
+      <h2>Registro de Nuevo Usuario</h2>
+      <form className='card card-body' onSubmit={handleRegister}>
+        <div className='form-group input-group'>
+        <div className='input-group-text bd-light'>
+            <i className='material-icons'>group_add</i>
+          </div>
+          <input className='form-control float-start' type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className='form-group input-group'>
+        <div className='input-group-text bd-light'>
+            <i className='material-icons'>group_add</i>
+          </div>
+          <input className='form-control float-start' type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button className='btn btn-primary btn-block' type="submit">Registrarse</button>
+      </form>
+    </div>
+  );
+}
+export default RegisterForm;
---
+++ src/protegido/sistemacrud/ListaDeEgresados.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ListaDeEgresados = () => {
+  return (
+    <div id="sis">
+      <h1>ListaDeEgresados</h1>
+      Lisat de Egresados...
+      <p>pepino aguilar</p>
+      <p>china del 71</p>
+      <p>pamela del8</p>
+      <p>julio del 71</p>
+      <p>maryha del 7</p>
+    </div>
+  )
+}
+
+export default ListaDeEgresados
